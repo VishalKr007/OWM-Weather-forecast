3bae856d670f044fa685584b25a42b0f74001acf
VishalKr007 <vishukr785@gmail.com>
2018-03-25 01:57:38 +0530

    Initial commit

--- weather.py
+++ weather.py
@@ -1,13 +1,36 @@
 import requests
 
-city = input('Enter your city name: ')
-url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=697ef311d890e8881867fa67b8312407'
-data = requests.get(url)
-read = data.json()
+def weather(city):
+    print("Fetching weather data....\n")
+    url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=697ef311d890e8881867fa67b8312407'
+    data = requests.get(url)
+    read_data = data.json()
+    print('city name: {}'.format(read_data['name']))
+    print('temperature: {}'.format(read_data['main']['temp']))
+    print('pressure: {}'.format(read_data['main']['pressure']))
+    print('humidity: {}'.format(read_data['main']['humidity']))
+    print('min-temp: {}'.format(read_data['main']['temp_min']))
+    print('max-temp: {}'.format(read_data['main']['temp_max']))
+#    print('sunset: {}'.format(read_data['main']['sunset']))
 
-print('city name: {}'.format(read['name']))
-print('temperature: {}'.format(read['main']['temp']))
-print('pressure: {}'.format(read['main']['pressure']))
-print('humidity: {}'.format(read['main']['humidity']))
-print('min-temp: {}'.format(read['main']['temp_min']))
-print('max-temp: {}'.format(read['main']['temp_max']))
+def main():
+    yourcity()
+
+
+def yourcity():
+    cityname = input('Please enter your city: ')
+    print('your city name is: ', cityname)
+    print("")
+    print('Do you want to proceed (y/n)')
+    x = input()
+    if x=="y":
+        weather(cityname)
+    else:
+        print('process terminated!!!')
+
+
+
+
+
+if __name__=='__main__':
+    main()
